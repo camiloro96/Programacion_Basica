105fc8eff1d7e7a9cdf7465622089accfbce6415
Cristian <camiloro96@outlook.com>
2018-10-10 19:47:01 -0500

    Avance

--- Canvas/dibujo.js
+++ Canvas/dibujo.js
@@ -1,9 +1,9 @@
-var d = document.getElementById("dibujito"); //getElementById metodo para obtener id en document
-var lienzo = d.getContext("2d");
+var d = document.getElementById("dibujito"); //getElementById metodo del document trae elemento
+var lienzo = d.getContext("2d"); //getContext metodo canvas
 
-lienzo.beginPath(); //Iniciar trazo en JS
-lienzo.strokeStyle = "red"; //Atributo para asignar color
-lienzo.moveTo(100, 100); //Metodo para mover el lapiz donde inicia la linea en X y Y 
-lienzo.lineTo(200, 200); //Funcion trazar linea hasta X y Y
+lienzo.beginPath(); //Metodo iniciar trazo
+lienzo.strokeStyle = "blue"; //Atributo para asignar color
+lienzo.moveTo(50, 10); //Metodo para mover el lapiz donde inicia la linea en X y Y 
+lienzo.lineTo(250, 2); //Funcion trazar linea hasta X y Y
 lienzo.stroke(); //Dibujar estilo 
 lienzo.closePath(); //Metodo para finalizar trazado
