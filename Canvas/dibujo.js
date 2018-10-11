9a47efa26f2af22b26e327b31f1dea2ed5e47398
Cristian <camiloro96@outlook.com>
2018-10-11 12:12:27 -0500

    Avance

--- Canvas/dibujo.js
+++ Canvas/dibujo.js
@@ -1,4 +1,4 @@
-var d = document.getElementById("dibujito"); //getElementById metodo del document trae elemento
+﻿var d = document.getElementById("dibujito"); //getElementById metodo del document trae elemento
 var lienzo = d.getContext("2d"); //getContext metodo canvas
 var lineas = 30;
 var l = 0; 
@@ -9,18 +9,18 @@ var colorcito = "#FAA";
 // Tamaño canvas 300px * 300px                                  0   0   0   0
 //                                                   0,0  __Y___|___|___|___|________    300,0
 //                                                          |                        |
-    //xi    yi      l       //xInicial  yInicial     0,10 __|                        |__ 10, 300
+    //xi    yi      l       //xInicial  yInicial     0,10 __|                        |__ 10,300
     //0     0       0       yi= 10*l                        |                        |  
-    //0     10      1                                0,20 __|                        |__ 20, 300  
-    //0     30      2                                       |                        |
-    //0     40      3                                0,30 __|                        |__ 30, 300
+    //0     10      1                                0,20 __|                        |__ 20,300  
+    //0     20      2                                       |                        |
+    //0     30      3                                0,30 __|                        |__ 30,300
 //                                                          |                        |  
     //xf    yf      l       //xFinal    yFinal              |                        |  
     //10    300     0       xf= 10*(l+1)                    |                        |
     //20    300     1                                       |________________________|  300,300
-    //30    300     2                                           |   |   |   |         X
-    //40    300     3                                          10, 20, 30, 40,                                     
-//                                                             300 300 300 300                                                           
+    //30    300     2                                       |   |   |   |   |         X
+    //40    300     3                                       0,  10, 20, 30, 40,                                     
+//                                                          300 300 300 300 300                                                           
 
 for(l = 0; l < lineas; l++) //varibale l empieza en 0 itera, mientras variable l sea menor que lineas (30), sumale 1
 {
