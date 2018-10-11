890a0018038c9cf161593f877c34c298a040bd55
Cristian <camiloro96@outlook.com>
2018-10-10 21:11:41 -0500

    Avance

--- Canvas/dibujo.js
+++ Canvas/dibujo.js
@@ -5,19 +5,20 @@ var l = 0;
 var yi, xf;
 var colorcito = "#FAA";
 
-// Tamaño canvas 300px * 300px
-//                                                   0,0  __ X 
-//                                                          |
-    //xi    yi      l       //xInicial  yInicial     0,10 __|
-    //0     0       0       yi= 10*l                        |
-    //0     10      1                                0,20 __|
-    //0     30      2                                       |
-    //0     40      3                                0,30 __|   
-//                                                          |
-    //xf    yf      l       //xFinal    yFinal              |  
-    //10    300     0       xf= 10*(l+1)                    |  
-    //20    300     1                                       |____________________ 300,300
-    //30    300     2                                           |   |   |   |    Y
+//                                                             10, 20, 30, 40,
+// Tamaño canvas 300px * 300px                                  0   0   0   0
+//                                                   0,0  __Y___|___|___|___|________    300,0
+//                                                          |                        |
+    //xi    yi      l       //xInicial  yInicial     0,10 __|                        |__ 10, 300
+    //0     0       0       yi= 10*l                        |                        |  
+    //0     10      1                                0,20 __|                        |__ 20, 300  
+    //0     30      2                                       |                        |
+    //0     40      3                                0,30 __|                        |__ 30, 300
+//                                                          |                        |  
+    //xf    yf      l       //xFinal    yFinal              |                        |  
+    //10    300     0       xf= 10*(l+1)                    |                        |
+    //20    300     1                                       |________________________|  300,300
+    //30    300     2                                           |   |   |   |         X
     //40    300     3                                          10, 20, 30, 40,                                     
 //                                                             300 300 300 300                                                           
 
@@ -29,7 +30,16 @@ for(l = 0; l < lineas; l++) //varibale l empieza en 0 itera, mientras variable l
     console.log("Linea " + l);
     //l++ = l+1
 }
-
+/*
+for(l = 0; l < lineas; l++) //varibale l empieza en 0 itera, mientras variable l sea menor que lineas (30), sumale 1
+{
+    yi = 10 * l; //10px cada linea
+    xf = 10 * (l+1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("Linea " + l);
+    //l++ = l+1
+}
+*/
 dibujarLinea(colorcito, 1, 1, 1, 299);
 dibujarLinea(colorcito, 1, 299, 299, 299);
 
