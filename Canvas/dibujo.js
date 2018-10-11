dfdcb3dd11b901f473328fc175f04bad6d2bccd6
Cristian <camiloro96@outlook.com>
2018-10-11 13:00:27 -0500

    Avance

--- Canvas/dibujo.js
+++ Canvas/dibujo.js
@@ -1,9 +1,45 @@
-﻿var d = document.getElementById("dibujito"); //getElementById metodo del document trae elemento
+﻿var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
+var d = document.getElementById("dibujito"); //getElementById metodo del document trae elemento
+var ancho = d.width; //width del canvas
 var lienzo = d.getContext("2d"); //getContext metodo canvas
-var lineas = 30;
-var l = 0; 
-var yi, xf;
-var colorcito = "#FAA";
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) //se declara nueva funcion
+{
+    lienzo.beginPath(); //Metodo iniciar trazo
+    lienzo.strokeStyle = color; //Atributo para asignar color
+    lienzo.moveTo(xinicial, yinicial); //Metodo para mover el lapiz donde inicia la linea en X y Y 
+    lienzo.lineTo(xfinal, yfinal); //Funcion trazar linea hasta X y Y
+    lienzo.stroke(); //Dibujar estilo 
+    lienzo.closePath(); //Metodo para finalizar trazado
+}
+
+function dibujoPorClick()
+{
+    var xx = parseInt(texto.value);
+    var lineas = xx;
+    var l = 0; 
+    var yi, xf;
+    var colorcito = "#FAA";
+    var espacio = ancho / lineas 
+
+    for(l = 0; l < lineas; l++) //varibale l empieza en 0 itera, mientras variable l sea menor que lineas (30), sumale 1
+    {
+        yi = espacio * l; //10px cada linea
+        xf = espacio * (l+1);
+        dibujarLinea(colorcito, 0, yi, xf, 300);
+        console.log("Linea " + l);
+        //l++ = l+1
+    }
+
+    dibujarLinea(colorcito, 1, 1, 1, 299);
+    dibujarLinea(colorcito, 1, 299, 299, 299);
+
+
+}
+
 
 //                                                             10, 20, 30, 40,
 // Tamaño canvas 300px * 300px                                  0   0   0   0
@@ -21,34 +57,3 @@ var colorcito = "#FAA";
     //30    300     2                                       |   |   |   |   |         X
     //40    300     3                                       0,  10, 20, 30, 40,                                     
 //                                                          300 300 300 300 300                                                           
-
-for(l = 0; l < lineas; l++) //varibale l empieza en 0 itera, mientras variable l sea menor que lineas (30), sumale 1
-{
-    yi = 10 * l; //10px cada linea
-    xf = 10 * (l+1);
-    dibujarLinea(colorcito, 0, yi, xf, 300);
-    console.log("Linea " + l);
-    //l++ = l+1
-}
-/*
-for(l = 0; l < lineas; l++) //varibale l empieza en 0 itera, mientras variable l sea menor que lineas (30), sumale 1
-{
-    yi = 10 * l; //10px cada linea
-    xf = 10 * (l+1);
-    dibujarLinea(colorcito, 0, yi, xf, 300);
-    console.log("Linea " + l);
-    //l++ = l+1
-}
-*/
-dibujarLinea(colorcito, 1, 1, 1, 299);
-dibujarLinea(colorcito, 1, 299, 299, 299);
-
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) //se declara nueva funcion
-{
-    lienzo.beginPath(); //Metodo iniciar trazo
-    lienzo.strokeStyle = color; //Atributo para asignar color
-    lienzo.moveTo(xinicial, yinicial); //Metodo para mover el lapiz donde inicia la linea en X y Y 
-    lienzo.lineTo(xfinal, yfinal); //Funcion trazar linea hasta X y Y
-    lienzo.stroke(); //Dibujar estilo 
-    lienzo.closePath(); //Metodo para finalizar trazado
-}
