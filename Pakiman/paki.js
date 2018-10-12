32961b8a6ece959e7b01bae1e0c811e99142b0ba
Cristian <camiloro96@outlook.com>
2018-10-12 17:50:23 -0500

    avance

---
+++ Pakiman/paki.js
@@ -0,0 +1,11 @@
+class Pakiman
+{
+    constructor(n) //funciona igual que Function
+    {
+        this.nombre = n;
+        this.tipo = "tierra";
+    }
+}
+
+var cauchin = new Pakiman("Cauchin");
+console.log(cauchin);
